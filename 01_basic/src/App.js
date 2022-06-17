f87a4dcaf8ac69190355c5813f31d12213c2254e
ajs3801 <tt14an@gmail.com>
2022-06-17 22:19:11 +0900

    filter the name

--- 01_basic/src/App.js
+++ 01_basic/src/App.js
@@ -4,14 +4,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  // 1
   constructor() {
     super();
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
+  // 3
   componentDidMount() { //딱 한번만 실행, mounting
     fetch('https://jsonplaceholder.typicode.com/users') //get API
     .then((response) => response.json())
@@ -20,17 +23,29 @@ class App extends Component{
         () => {
           return {monsters: users}; //put data to monsters
         },
-        () => {
-          console.log(this.state);
-        }
       )
     ) //json으로 변환된 것은 users로 들어감
   }
-
+  
+  // 2
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+
     return (
       <div className="App">
-        {this.state.monsters.map(
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          console.log(event.target.value)
+          
+          const searchField = event.target.value.toLocaleLowerCase();
+
+          this.setState(() => {
+            return {searchField};
+          })
+        }}/> {/* event handler onChange | event : get event */}
+     
+        {filteredMonsters.map(
           (monster) => {
             return (
             <div key={monster.id}>
