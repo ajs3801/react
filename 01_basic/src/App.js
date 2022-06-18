fe01f9f58a4e3d70e0315dd23dc87f38f5d5cccf
ajs3801 <tt14an@gmail.com>
2022-06-18 14:33:39 +0900

    Optimizations

--- 01_basic/src/App.js
+++ 01_basic/src/App.js
@@ -4,17 +4,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
-  // 1
+  // [First Execution] : 초기값 할당
   constructor() {
     super();
-
+    
+    // setState를 호출하는 것이 아닌 this.state으로 초기값을 할당해야함
     this.state = {
       monsters: [],
       searchField: '',
     };
   }
 
-  // 3
+  // [Third Execution] : 외부 API를 받아옴
   componentDidMount() { //딱 한번만 실행, mounting
     fetch('https://jsonplaceholder.typicode.com/users') //get API
     .then((response) => response.json())
@@ -27,23 +28,31 @@ class App extends Component{
     ) //json으로 변환된 것은 users로 들어감
   }
   
-  // 2
+  //init only once
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return {searchField}; // searchField : searchField
+      }
+    )
+  }
+
+  // DOM을 update할 때마다 render함수를 call
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    // use this variable
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    // const { onSearchChange } = this.onSearchChange;로는 불가능?
+
+    const filteredMonsters = monsters.filter((monster) => {
+      // monster의 이름이 searchField를 포함하고 있으면 filteredMonsters에 넣음
+      return monster.name.toLocaleLowerCase().includes(searchField);
     })
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          console.log(event.target.value)
-          
-          const searchField = event.target.value.toLocaleLowerCase();
-
-          this.setState(() => {
-            return {searchField};
-          })
-        }}/> {/* event handler onChange | event : get event */}
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/> {/* event handler onChange | event : get event */}
      
         {filteredMonsters.map(
           (monster) => {
